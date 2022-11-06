fb8bea57d487c605543695b4df1c0a4fb8865774
Amidou Kante <kamdiou@gmail.com>
2022-11-06 08:15:03 -0800

    update account changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,28 @@ import "../styles/globals.scss";
 
 import { WagmiConfig, createClient } from "wagmi";
 import { getDefaultProvider } from "ethers";
-import { ApolloProvider } from "@apollo/client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { SessionProvider } from "next-auth/react";
 import { LensProvider } from "../hooks/useLens";
+
+
+const httpLink = createHttpLink({
+  uri: "https://api.lens.dev",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('lens')
+  const { expires, access } = JSON.parse(token) || {};
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      'x-access-token': access?.authenticate?.accessToken,
+    }
+  }
+});
 
 const _client = createClient({
   autoConnect: true,
@@ -13,12 +31,8 @@ const _client = createClient({
 });
 
 const apolloClient = new ApolloClient({
-  uri: "https://api.lens.dev",
   cache: new InMemoryCache(),
-  headers: {
-    "x-access-token":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjB4M0EzOTI2QjBGZTg4QTAzZThBNmJBNTkxQzhlYjA2MTJGMjA1M0M5YyIsInJvbGUiOiJub3JtYWwiLCJpYXQiOjE2Njc3MjMzNzksImV4cCI6MTY2NzcyNTE3OX0.E7DEqZbUU6J0OIZ3H35L7bQPTZsFr-ZDJZRJ2Whysrc",
-  },
+  link: authLink.concat(httpLink)
 });
 
 function MyApp({ Component, pageProps }) {
