40c91f257d516e867651d6b2a318e38a6189610a
0xbryz <0xbryz@gmail.com>
2022-11-06 13:29:40 -0800

    move twitter buttons

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import styles from "../styles/Welcome.module.scss";
 import Account from "./Account";
 import { Disconnect } from "./Connect";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Lens from "./LensConnect";
 import Tweets from "./Tweets";
+import Button from "../components/Button";
 
 export default function Welcome() {
+  const session = useSession();
   return (
     <div className={styles.welcome}>
       <div className={styles.disconnect}>
@@ -14,6 +17,17 @@ export default function Welcome() {
       <div className={styles.container}>
         <Account />
         <Lens />
+        {!session.data && (
+          <>
+            <Button onClick={() => signIn()}>Sign in with Twitter</Button>
+          </>
+        )}
+        {session.data && (
+          <>
+            Signed in as @{session.data?.user?.name} <br />
+            <Button onClick={() => signOut()}>Sign out</Button>
+          </>
+        )}
         <div className={styles.cardContainer}>
           <Tweets />
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+
 import { useAccount } from "wagmi";
 import Intro from "../components/Intro";
 import styles from "../styles/Home.module.scss";
@@ -7,10 +7,8 @@ import Welcome from "../components/Welcome";
 
 export default function Home() {
   const { isConnected } = useAccount();
-  const session = useSession();
-  const [connect, setConnect] = useState();
 
-  console.log({ session });
+  const [connect, setConnect] = useState();
 
   useEffect(() => {
     setConnect(isConnected);
@@ -20,20 +18,7 @@ export default function Home() {
     <div>
       {!connect && <Intro />}
       {connect && <Welcome />}
-      <div className={styles.container}>
-        {!session.data && (
-          <>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-          </>
-        )}
-        {session.data && (
-          <>
-            Signed in as {session.data?.user?.name} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </>
-        )}
-      </div>
+      <div className={styles.container}></div>
     </div>
   );
 }
