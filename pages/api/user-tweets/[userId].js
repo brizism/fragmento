23394698cd94c72ff6eeae3176a79a2354f74269
Amidou Kante <kamdiou@gmail.com>
2022-11-05 21:38:40 -0700

    modify api

--- pages/api/user-tweets/[userId].js
+++ pages/api/user-tweets/[userId].js
@@ -5,7 +5,7 @@ const headers = {
   "Authorization": `Bearer ${process.env.TWITTER_BEARER}`
 }
 
-const baseURL = (userId) => `https://api.twitter.com/2/users/${userId}/tweets?max_results=100&tweet.fields=text&user.fields=id`
+const baseURL = (userId) => `https://api.twitter.com/2/users/${userId}/tweets?max_results=100&tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,lang,possibly_sensitive,source,text&media.fields=alt_text,height,preview_image_url,url,width`
 
 async function main(userId = '127933832') {
   const data = await (await axios.get(baseURL(userId), { headers })).data;
